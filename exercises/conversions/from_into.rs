90ffc9c0b4d2b592de43074f15cae4b643136afc
Fabricio Asfora <fabricio_asfora_002@hotmail.com>
2023-07-29 11:59:01 -0300

    Update from_into.rs

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,23 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let results = s.split(",").collect::<Vec<_>>();
+        if results.len() != 2 || results[0].is_empty() || results[1].is_empty() {
+            return Person::default();
+        }
+        let Ok(age) = results[1].parse::<usize>() else{
+            return Person::default();
+        };
+        let mut p = Person::default();
+        p.name = results[0].to_owned();
+        p.age = age;
+        return p;
     }
 }
 
